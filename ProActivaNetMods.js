cde445a9db53fbf8b22508dd9a19ea1e0e6247f4
hirkkak <hirkkak@hotmail.com>
2026-07-13 14:28:27 +0200

    Update ProActivaNetMods.js

--- ProActivaNetMods.js
+++ ProActivaNetMods.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         ProActivaNet Mario Mods
 // @namespace    ProactivaNet
-// @version      2026-04-11
+// @version      2026-07-13
 // @description  Modificaciones de Mario para proActivaNet
 // @author       Hirkkak
 // @match        */proactivanet/servicedesk/incidents/formIncidents/formIncidents.paw*
+// @match        */proactivanet/servicedesk/incidents/allIncidents/allIncidents.paw*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=147.39
 // @require      https://code.jquery.com/jquery-3.6.0.min.js
 // @grant        GM_getValue
@@ -12,6 +13,8 @@
 // @grant        GM_log
 // @grant        GM_xmlhttpRequest
 // @grant        GM.xmlHttpRequest
+// @grant        GM_addValueChangeListener
+// @grant        unsafeWindow
 //
 // @require      https://github.com/hirkkak/ProactivaNet_Mods/raw/main/ProActivaNetModsHelper.js
 // @supportURL   https://github.com/hirkkak/ProactivaNet_Mods/issues
@@ -19,10 +22,65 @@
 // @updateURL    https://github.com/hirkkak/ProactivaNet_Mods/raw/main/ProActivaNetMods.js
 // ==/UserScript==
 
-//const MODIFICACIONES_ACTIVADAS = "StopContinueBtn";
 var scriptActivo = localStorage.getItem("StopContinueBtn");
-
 (function() {
+    //console.clear();
+    var $ = window.jQuery;
+
+
+    // Solo para la Lista de incidencias
+    if (window.location.href.indexOf("allIncidents") > 0){
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+const JQ_FILTRO = 'input.pawPTableToolBarSelEdit';
+const JQ_FILTRO_ICON = 'button.pawPTableToolBarSelEditBtn';
+
+function ejecutarFiltroFecha() {
+    // Si el buscador real no está en este frame, no hacemos nada
+    if ($(JQ_FILTRO).length === 0) return;
+
+    const fechaGuardada = GM_getValue("miFecha");
+    if (!fechaGuardada) return;
+
+    const [year, month, day] = fechaGuardada.split('-');
+    if (!year || !month || !day) return;
+
+    const TEXTO_BUSCAR = `${day}/${month}/${year}`;
+
+    // 1. Escribimos el valor usando jQuery (como te funcionaba al principio)
+    $(JQ_FILTRO).val(TEXTO_BUSCAR);
+    console.log("Script 1: Aplicando filtro ->", TEXTO_BUSCAR);
+
+    // 2. Hacemos el clic automático con un margen para que procese el texto
+    setTimeout(() => {
+        if ($(JQ_FILTRO_ICON).length > 0) {
+            $(JQ_FILTRO_ICON).trigger("click");
+            console.log("Script 1: ¡Clic enviado con jQuery!");
+        } else {
+            console.warn("Script 1: No se encontró el botón del filtro para hacer clic.");
+        }
+    }, 300);
+}
+
+
+// Escuchador de Tampermonkey (el que asegura la mensajería entre ambos)
+if (typeof GM_addValueChangeListener === "function") {
+    GM_addValueChangeListener("miFecha", function() {
+        console.log("Script 1: Cambio de fecha recibido en este frame.");
+        ejecutarFiltroFecha();
+    });
+}
+
+// Ejecución inicial por si acaso al cargar
+ejecutarFiltroFecha();
+
+        //////////////////////////////////////////////////////////////////////////////////////////
+        //////////////////////////////////////////////////////////////////////////////////////////
+
+     //return;
+    }
+
     'use strict';
     //const JQ_BOTON_MES_SIGUIENTE = 'td.pawCalT1[paw\\:cmd^="nxm"]';
     const JQ_COMBO_TAREAS = 'table#padTypes_id';
@@ -45,13 +103,7 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
     scriptActivo = (scriptActivo === "true");
     if (!scriptActivo) return;
 
-    //console.clear();
-
-    var $ = window.jQuery;
-    //this.$ = this.window.jQuery = window.jQuery.noConflict(true);
-    //this.$ = this.jQuery = jQuery.noConflict(true);
     //Array con los id de los campos que se ocultaran
-
     var OBJ_OCULTOS =[];
 
     var ALL_READY = false;
@@ -111,7 +163,33 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
             }
         }, 0);
         //getHoras();
+
+
+// 🔥 REEMPLAZO SEGURO: Escuchamos el cambio nativo de Tampermonkey en lugar del CustomEvent
+if (typeof GM_addValueChangeListener === "function") {
+    GM_addValueChangeListener("miFecha", function(key, oldValue, newValue, remote) {
+        console.log("Script 1 (Ready): Cambio de fecha detectado ->", newValue);
+
+        // Ejecutamos tu setFecha() sacándolo del hilo principal para evitar bloqueos del DOM
+        setTimeout(function() {
+            if (typeof setFecha === "function") {
+                setFecha();
+                console.log("Script 1: setFecha() ejecutada con éxito de forma local.");
+            } else if (typeof window.setFecha === "function") {
+                window.setFecha();
+                console.log("Script 1: setFecha() ejecutada con éxito vía window.");
+            } else {
+                console.error("Script 1: No se puede acceder a setFecha() desde este bloque.");
+            }
+        }, 50);
+    });
+}
+
+
         console.log("Fin del script.");
+
+
+
     });
 
     //var autocheck = setTimeout(modificarCombo, 1000);
@@ -160,7 +238,7 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
         //Click en combo trgsa para ocultar
         $("span.pawDFSelPopup").click(function(){
             contClick++;
-            if (contClick>1) {                
+            if (contClick>1) {
                 checkBotonAceptar();
             }
             //console.log("Click");
@@ -193,6 +271,7 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
         }
     }
 
+    //window.setFecha = function()
     function setFecha()
     {
         const JQ_FECHA = 'table#creationDate.pawDFMultiFunReq';
@@ -271,9 +350,49 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
         }, 0);
     }
 
+    function sumarRestarMinutos(minutes)
+    {
+        const JQ_Min  = "#fldPadIncidents_DedicatedHoursFirstLine_M"
+        const JQ_Hour = "#fldPadIncidents_DedicatedHoursFirstLine_H"
+        const idCheckBtnRestar = "checkBtnRestar";
+
+        var restar = $("#" + idCheckBtnRestar).is(":checked");
+
+        if ($(JQ_Hour).val() == 0) $(JQ_Hour).val(0);
+        if ($(JQ_Min).val() == 0) $(JQ_Min).val(0);
+
+        var hour = parseInt($(JQ_Hour).val());
+        var min = parseInt($(JQ_Min).val());
+
+        var totalMinutes = (hour * 60) + min;
+
+        if (restar) //Resta
+        {
+            totalMinutes = totalMinutes - minutes;
+            if (totalMinutes < 0) { totalMinutes = 0; }
+        }
+        else
+        {
+            totalMinutes = totalMinutes + minutes;
+        }
+
+        var newHour = parseInt(totalMinutes/60);
+        var newMinutes = totalMinutes - (60*newHour);
+
+        $(JQ_Min).val(newMinutes);
+        $(JQ_Min).trigger('change');
+        $(JQ_Hour).val(newHour);
+        $(JQ_Hour).trigger('change');
+
+    }
+
     function crearBotonesSumarTiempo(){
 
         var idObjOcultar = "fldPadIncidents_OtherExpensesFirstLine";
+        const idCheckBtnRestar = "checkBtnRestar";
+        const bgColorSumar = "DarkSeaGreen";
+        const bgColorRestar = "LightCoral";
+
         const JQ_Min = "#fldPadIncidents_DedicatedHoursFirstLine_M"
         const JQ_Hour = "#fldPadIncidents_DedicatedHoursFirstLine_H"
 
@@ -283,52 +402,76 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
         //Oculta el campo de precio euros
         objOcultar[0].style.display = "none";
 
-     //Botones de minutos y horas
-        var $inputButton15 = $('<input type="button" value="+15\'" />');
+
+        //Botones de minutos y horas
+        var $inputButton15 = $('<input type="button" value="15\'" />');
         $inputButton15.css("position", "relative");
         $inputButton15.css("left", "20px");
+        $inputButton15.css("background-color", bgColorSumar);
         $inputButton15.click( function (){
-            if ($("#fldPadIncidents_DedicatedHoursFirstLine_M").val() == 0) $("#fldPadIncidents_DedicatedHoursFirstLine_M").val(0);
-            var newValue = parseInt($("#fldPadIncidents_DedicatedHoursFirstLine_M").val())+15;
-            $("#fldPadIncidents_DedicatedHoursFirstLine_M").change();
-            $(JQ_DIA).trigger( "click" );
-            $("#fldPadIncidents_DedicatedHoursFirstLine_M").val(newValue);
+            sumarRestarMinutos (15);
             checkBotonAceptar();
         });
         $inputButton15.appendTo(objDestino[0]);
 
-        var $inputButton30 = $('<input type="button" value="+30\'" />');
+        var $inputButton30 = $('<input type="button" value="30\'" />');
         $inputButton30.css("position", "relative");
         $inputButton30.css("left", "30px");
+        $inputButton30.css("background-color", bgColorSumar);
         $inputButton30.click( function (){
-            if ($("#fldPadIncidents_DedicatedHoursFirstLine_M").val() == 0) $("#fldPadIncidents_DedicatedHoursFirstLine_M").val(0);
-            var newValue = parseInt($("#fldPadIncidents_DedicatedHoursFirstLine_M").val())+30;
-            $("#fldPadIncidents_DedicatedHoursFirstLine_M").val(newValue);
+            sumarRestarMinutos (30);
             checkBotonAceptar();
         });
         $inputButton30.appendTo(objDestino[0]);
 
-        var $inputButton1H = $('<input type="button" value="+1H" />');
+        var $inputButton1H = $('<input type="button" value="1H" />');
         $inputButton1H.css("position", "relative");
         $inputButton1H.css("left", "40px");
+        $inputButton1H.css("background-color", bgColorSumar);
         $inputButton1H.click( function (){
-            if ($(JQ_Hour).val() == 0) $(JQ_Hour).val(0);
-            var newValue = parseInt($(JQ_Hour).val())+1;
-            $(JQ_Hour).val(newValue);
+            sumarRestarMinutos (60);
             checkBotonAceptar();
         });
         $inputButton1H.appendTo(objDestino[0]);
 
-        var $inputButton3H = $('<input type="button" value="+3H" />');
+        var $inputButton3H = $('<input type="button" value="3H" />');
         $inputButton3H.css("position", "relative");
         $inputButton3H.css("left", "50px");
+        $inputButton3H.css("background-color", bgColorSumar);
         $inputButton3H.click( function (){
-            if ($(JQ_Hour).val() == 0) $(JQ_Hour).val(0);
-            var newValue = parseInt($(JQ_Hour).val())+3;
-            $(JQ_Hour).val(newValue);
+            sumarRestarMinutos (180);
             checkBotonAceptar();
         });
         $inputButton3H.appendTo(objDestino[0]);
+
+        //CheckButton Restar
+        var $checkBtnRestar = $('<input type="checkbox" value="True"/>');
+        $checkBtnRestar.attr({id: idCheckBtnRestar});
+
+        $checkBtnRestar.change( function (){
+            var restar = $("#" + idCheckBtnRestar).is(":checked");
+            var bgColor = bgColorSumar;
+            if (restar) { bgColor = bgColorRestar; }
+
+            $inputButton15.css("background-color", bgColor);
+            $inputButton30.css("background-color", bgColor);
+            $inputButton1H.css("background-color", bgColor);
+            $inputButton3H.css("background-color", bgColor);
+
+        });
+
+        //$checkBtnRestar.css("position", "relative");
+        //$checkBtnRestar.css("left", "80px");
+        //$checkBtnRestar.appendTo(objDestino[0]);
+
+        var $lblCheckBtnRestar = $("<label>").text('Restar');
+        $lblCheckBtnRestar.css("position", "relative");
+        $lblCheckBtnRestar.css("left", "65px");
+        $lblCheckBtnRestar.appendTo(objDestino[0]);
+
+        //Insert the input into the label
+        $checkBtnRestar.appendTo($lblCheckBtnRestar);
+
     }
     function crearBotonesOpcionales()
     {
@@ -348,7 +491,7 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
 
         //GM_setValue("miFecha", "2025/07/01");
 
-        var $inputFecha = $('<input id="inputFecha" type="text" value="' + GM_getValue("miFecha") + '" />');
+        /*var $inputFecha = $('<input id="inputFecha" type="text" value="' + GM_getValue("miFecha") + '" />');
 
         $("<span class='pawTrackerLast'> - Fecha (yyyy/mm/dd): </span>").appendTo(objDestino[0]);
         $inputFecha.width (80);
@@ -359,7 +502,31 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
             setFecha();
             //console.log(GM_getValue("miFecha"));
             //window.miFecha = $inputFecha;
-        });
+        });*/
+/*
+var $inputFecha = $('<input id="inputFecha" type="date" value="' + fechaFormateada + '" />');
+
+$("<span class='pawTrackerLast'> - Fecha: </span>").appendTo(objDestino[0]);
+$inputFecha.width(130);
+$inputFecha.appendTo(objDestino[0]);
+
+// Evento de cambio para el Input 2
+$("#inputFecha").change(function () {
+    var nuevoValor = $inputFecha.val(); // "yyyy-mm-dd"
+
+    GM_setValue("miFecha", nuevoValor);
+
+    // 🔄 SINCRONIZAR: Actualizamos el Input 1 (Nativo) si existe en la página
+    var input1 = document.getElementById("gmFechaInput");
+    if (input1) {
+        input1.value = nuevoValor;
+    }
+
+    // Ejecutamos tu función de actualización
+    if (typeof setFecha === "function") setFecha();
+});*/
+
+
 
         //Botones de HOY
         var $inputButtonHoy = $('<input type="button" value="HOY" />');
@@ -382,9 +549,31 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
     }
 
     function modificarComboTragsa(){
-        //var elem = $(document).contents().children(2).contents().find("span" );
-        //var idBuscar = "viewAllIncidents_padTypes_id_Selector"
-        //var idBuscar = "6fa662c8-85ac-4192-bff8-a193649b893d"
+
+        /*
+        const COLOR_1 = "DarkKhaki";
+        const COLOR_2 = "Thistle";
+        const COLOR_3 = "LightCyan";
+        const COLOR_4 = "Bisque";
+        const COLOR_5 = "Peru";
+        const COLOR_6 = "Chocolate";
+        */
+
+        const COLOR_1 = "#EDFCFF";
+
+        const COLOR_2 = "#D2EBC3";
+        const COLOR_2_B = "#BDE8A2";
+
+        const COLOR_3 = "#E2E86F";
+        const COLOR_3_B = "#E6E8A0";
+        const COLOR_3_C = "#D9C148";
+
+        const COLOR_4 = "#C3DBEB";
+        const COLOR_4_B = "#A7CCE8";
+
+        const COLOR_5 = "#FFEDED";
+        const COLOR_6 = "#FAE3E3";
+
         var classBuscar = "pawDFSelPopup"
         var elems = window.document.getElementsByTagName("span");
         for (var i in elems){
@@ -411,30 +600,6 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
                                 var fontWeight = "normal";
                                 var fullNumero = texto.substring(1, 5);
 
-                                /*
-                                const COLOR_1 = "DarkKhaki";
-                                const COLOR_2 = "Thistle";
-                                const COLOR_3 = "LightCyan";
-                                const COLOR_4 = "Bisque";
-                                const COLOR_5 = "Peru";
-                                const COLOR_6 = "Chocolate";
-                                */
-
-                                const COLOR_1 = "#EDFCFF";
-
-                                const COLOR_2 = "#D2EBC3";
-                                const COLOR_2_B = "#BDE8A2";
-
-                                const COLOR_3 = "#D9EBCC";
-                                const COLOR_3_B = "#E6E8A0";
-
-                                const COLOR_4 = "#C3DBEB";
-                                const COLOR_4_B = "#A7CCE8";
-
-                                const COLOR_5 = "#FFEDED";
-                                const COLOR_6 = "#FAE3E3";
-
-
                                 //object.style.fontWeight = "normal|lighter|bold|bolder|value|initial|inherit"
 
                                 //console.log(numero);
@@ -451,10 +616,12 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
                                         else color = COLOR_2;
                                         break;
                                     case 3:
-                                        color = COLOR_3;
-                                        if (fullNumero != "3.03") {
-                                            fontWeight = "bold";
-                                            color = COLOR_3_B;
+                                        switch (fullNumero)
+                                        {
+                                            case "3.01": color = COLOR_3; fontWeight = "bold"; break;
+                                            case "3.02": color = COLOR_3_C; fontWeight = "bold"; break;
+                                            case "3.03": color = COLOR_3_B; break;
+                                            case "3.04": color = COLOR_3_B; fontWeight = "bold"; break;
                                         }
                                         break;
                                     case 4:
@@ -543,4 +710,6 @@ var scriptActivo = localStorage.getItem("StopContinueBtn");
         objDestino[0].append( " ");
         $inputButton.appendTo(objDestino[0]);
     }
+
+
 })();
